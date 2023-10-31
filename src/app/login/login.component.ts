c09753568bd6fe3fb15fc4df5158fb60c59cde26
luislabradaa <swirly.olive@gmail.com>
2023-10-30 23:27:37 -0600

    Componente Login v1.02 validaciones

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent  {
 
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email,Validators.maxLength(40)]),
+    password: new FormControl('', [Validators.required,Validators.minLength(8),Validators.maxLength(20)])
+  })
   constructor() { }
 
-  ngOnInit() {}
+ 
 
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+//Validaciones y formulario
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 import { LoginPageRoutingModule } from './login-routing.modules';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
@@ -10,6 +11,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     LoginPageRoutingModule
   ],
